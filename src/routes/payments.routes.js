f7587ec20a41dd2805f98439c318e4bc8f4708f8
2412002Ori <orianadaran1@gmail.com>
2025-06-19 12:09:43 -0400

    Revert "init prisma"
    This reverts commit ca2c85eb1f6d485f331fb7eb4a0daf379e627eb3.

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { postPayment , getAllPayments , getPaymentsById , patchPaymentsById , deletepaymentsById} from "../controlers/payments.controlers.js";
-import { authMiddleware} from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/payments", authMiddleware , postPayment);
-router.get("/payments", authMiddleware , getAllPayments);
-router.get("/payments/:id", authMiddleware , getPaymentsById);
-router.patch("/payments/:id", authMiddleware , patchPaymentsById);
-router.delete("/payments/:id", authMiddleware , deletepaymentsById);
+router.post("/payments", postPayment);
+router.get("/payments", getAllPayments);
+router.get("/payments/:id", getPaymentsById);
+router.patch("/payments/:id", patchPaymentsById);
+router.delete("/payments/:id", deletepaymentsById);
 
 export default router;
--- src/routes/tenants.routes.js
+++ src/routes/tenants.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { postTenants, getTenants, getTenantById , putTenantById,  } from "../controlers/tenants.controlers.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/tenants",authMiddleware , postTenants );
-router.get("/tenants", authMiddleware , getTenants);
-router.get("/tenants/:id", authMiddleware , getTenantById);
-router.put("/tenants/:id", authMiddleware , putTenantById);
+router.post("/tenants",postTenants );
+router.get("/tenants", getTenants);
+router.get("/tenants/:id", getTenantById);
+router.put("/tenants/:id", putTenantById); 
 
 
 export default router;
