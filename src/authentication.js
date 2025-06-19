8efe0b70921c908b72e76e18866132ef16d076fd
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-06-19 14:07:02 -0400

    cambios menores

--- src/authentication.js
+++ src/authentication.js
@@ -6,11 +6,11 @@ export function generarToken(payload) {
     return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
 }
 
-export default function verificarToken(token) {
-    try {
-        return jwt.verify(token, SECRET_KEY);
-    } catch (err) {
-        return null;
-    }
-}
+// export default function verificarToken(token) {
+//     try {
+//         return jwt.verify(token, SECRET_KEY);
+//     } catch (err) {
+//         return null;
+//     }
+// }
 
