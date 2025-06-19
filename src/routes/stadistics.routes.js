ca2c85eb1f6d485f331fb7eb4a0daf379e627eb3
2412002Ori <orianadaran1@gmail.com>
2025-06-19 11:25:40 -0400

    init prisma

--- src/routes/stadistics.routes.js
+++ src/routes/stadistics.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAlldata, getdataById } from "../controlers/stadisticts.controlers.js";  
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get("/stadistics/:year", getAlldata);
-router.get("/stadistics/:id/:year", getdataById);
+router.get("/stadistics/:year", authMiddleware , getAlldata);
+router.get("/stadistics/:id/:year", authMiddleware , getdataById);
 
 
 
--- src/routes/tenants.routes.js
+++ src/routes/tenants.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { postTenants, getTenants, getTenantById , putTenantById,  } from "../controlers/tenants.controlers.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/tenants",postTenants );
-router.get("/tenants", getTenants);
-router.get("/tenants/:id", getTenantById);
-router.put("/tenants/:id", putTenantById); 
+router.post("/tenants",authMiddleware , postTenants );
+router.get("/tenants", authMiddleware , getTenants);
+router.get("/tenants/:id", authMiddleware , getTenantById);
+router.put("/tenants/:id", authMiddleware , putTenantById);
 
 
 export default router;
