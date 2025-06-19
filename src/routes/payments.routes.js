b65161771cc7410b3a526e06b1ddd71026136955
2412002Ori <orianadaran1@gmail.com>
2025-06-19 14:29:09 -0400

    cambios

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { postPayment , getAllPayments , getPaymentsById , patchPaymentsById , deletepaymentsById} from "../controlers/payments.controlers.js";
 
 const router = Router();
 
-router.post("/payments", postPayment);
-router.get("/payments", getAllPayments);
-router.get("/payments/:id", getPaymentsById);
-router.patch("/payments/:id", patchPaymentsById);
-router.delete("/payments/:id", deletepaymentsById);
+router.post("/payments",authMiddleware , postPayment);
+router.get("/payments", authMiddleware , getAllPayments);
+router.get("/payments/:id", authMiddleware , getPaymentsById);
+router.patch("/payments/:id", authMiddleware , patchPaymentsById);
+router.delete("/payments/:id", authMiddleware , deletepaymentsById);
 
 export default router;
