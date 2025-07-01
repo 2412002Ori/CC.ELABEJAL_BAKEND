920a019f522cb5a2c191128f63ea1230e1bb3b71
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-01 18:38:40 -0400

    p

--- src/authentication.js
+++ src/authentication.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = 'C41'; // tengo que guardar la variable de entorno, aun no lo hago
+const SECRET_KEY = process.env.SECRET_KEY  
 
 export function generarToken(payload) {
     return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
