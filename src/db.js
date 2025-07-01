b3d125866063226a58c296c7e91914d04007f0c7
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-01 18:25:54 -0400

    P

--- src/db.js
+++ src/db.js
@@ -1,22 +1,3 @@
-// import { Pool } from 'pg';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from './config.js'
-
-// const pool = new Pool({
-//   user: DB_USER,
-//   host: DB_HOST,
-//   password: DB_PASSWORD,
-//   database: DB_DATABASE,
-//   port: DB_PORT,
-// });
-
-// pool.connect()
-//   .then(() => console.log('📦 Conexión a PostgreSQL exitosa'))
-//   //.catch(err => console.error('❌ Error al conectar a PostgreSQL', err));
-
-// export default pool;
-
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();;
