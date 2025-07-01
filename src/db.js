0cf3381a79597f96efc33628184b603f2336ee58
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-01 17:44:10 -0400

    p

--- src/db.js
+++ src/db.js
@@ -1,18 +1,41 @@
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from './config.js'
+
+// const pool = new Pool({
+//   user: DB_USER,
+//   host: DB_HOST,
+//   password: DB_PASSWORD,
+//   database: DB_DATABASE,
+//   port: DB_PORT,
+// });
+
+// pool.connect()
+//   .then(() => console.log('📦 Conexión a PostgreSQL exitosa'))
+//   //.catch(err => console.error('❌ Error al conectar a PostgreSQL', err));
+
+// export default pool;
+
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+dotenv.config();;
+
+// Cargar variables de entorno ANTES de acceder a ellas
 dotenv.config();
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from './config.js'
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
 
 const pool = new Pool({
-  user: DB_USER,
   host: DB_HOST,
+  user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_DATABASE,
-  port: DB_PORT,
+  database: DB_NAME,
+  port: Number(DB_PORT), // asegúrate que el puerto sea número
 });
 
 pool.connect()
   .then(() => console.log('📦 Conexión a PostgreSQL exitosa'))
-  //.catch(err => console.error('❌ Error al conectar a PostgreSQL', err));
+  .catch(err => console.error('❌ Error al conectar a PostgreSQL', err));
 
 export default pool;
