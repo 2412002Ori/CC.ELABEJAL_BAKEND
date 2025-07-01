0de277371c12e2f0658559e427c4ee5a00bd7f46
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-01 15:53:44 -0400

    cambios cesar main

---
+++ src/controlers/login.controlers.js
@@ -0,0 +1,35 @@
+import * as LoginModel from "../models/login.models.js";
+import { generarToken } from "../authentication.js";
+
+export async function login(req, res) {
+    const { username, password } = req.body;
+
+    try {
+        const user = await LoginModel.findUserByUsername(username);
+
+        if (!user) {
+            return res.status(401).json({ mensaje: "Usuario no encontrado" });
+        }
+        if (user.password !== password) {
+            return res.status(401).json({ mensaje: "Contraseña incorrecta" });
+        }
+
+        const token = generarToken({ id: user.user_id, username: user.username, role: user.role_id });
+        res.json({ 
+            mensaje: "Login exitoso",
+            usuario: {
+                id: user.user_id,
+                username: user.username,
+                role: user.role_id,
+                email: user.email,
+                name: user.name,
+                lastname: user.lastname,
+                status: user.status
+            },
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ mensaje: "Error en el servidor" });
+    }
+}
