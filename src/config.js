4048cdcab9b1f6aff41137e18867e1da3601a089
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-01 18:21:53 -0400

    P

--- src/config.js
+++ src/config.js
@@ -5,5 +5,4 @@ export const DB_DATABASE = process.env.DB_DATABASE
 export const DB_PORT = process.env.DB_PORT
 
 export const PORT = process.env.PORT || 3003;
-
 export const SECRET_KEY = process.env.SECRET_KEY
