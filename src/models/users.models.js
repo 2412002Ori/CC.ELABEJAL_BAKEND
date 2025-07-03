34bb98e551379d8ebb25e497cf197fd49daf256e
Cesarrc412004 <cesardanielrc41@gmail.com>
2025-07-03 04:43:55 -0400

    p

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -19,9 +19,9 @@ export async function createUser(user) {
 }
 
 export async function updateUser(id, user) {
-    const { username, email, password, role_id, created_at, updated_at, status, name, lastname } = user;
-    const { rows } = await pool.query('UPDATE users SET username = $1, email = $2, password = $3, role_id = $4, created_at = $5, updated_at = $6, status = $7, name = $8, lastname = $9 WHERE user_id = $10 RETURNING *',
-        [username, email, password, role_id, created_at, updated_at, status, name, lastname, id]
+    const { username, email, password, role_id, status, name, lastname } = user;
+    const { rows } = await pool.query('UPDATE users SET username = $1, email = $2, password = $3, role_id = $4, status = $5, name = $6, lastname = $7 WHERE user_id = $8 RETURNING *',
+        [username, email, password, role_id, status, name, lastname, id]
     );
     return rows[0];
 }
