9b395df5dc7dcda71d2b8f30e405ad67c6e55649
2412002Ori <orianadaran1@gmail.com>
2025-07-03 11:26:55 -0400

    Update authentication.js

--- src/authentication.js
+++ src/authentication.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 const SECRET_KEY = process.env.SECRET_KEY  
+if (!SECRET_KEY) {
+  throw new Error('SECRET_KEY no está definido en las variables de entorno. Por favor, configúralo en Render o en un archivo .env');
+}
 
 export function generarToken(payload) {
     return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
